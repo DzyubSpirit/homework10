31bbabc5d3d8e1e981d50f76bf949350d8ec8e30
DzyubSpirit <Dzyubavlad@gmail.com>
2019-03-25 14:11:27 +0000

    Implement system graph and opening-saving of graphs

--- swcs/lab1/app/Main.hs
+++ swcs/lab1/app/Main.hs
@@ -4,8 +4,11 @@ import Protolude hiding (on)
 import Conduit
 import Control.Monad.Reader (withReaderT)
 import Data.Conduit.Lift
+import qualified Data.ByteString.Lazy as B (writeFile, readFile)
 import Graphics.UI.Gtk hiding (Weight)
 import Graphics.UI.Gtk.Builder
+import Graphics.UI.Gtk.Selectors.FileChooser
+import Graphics.UI.Gtk.Selectors.FileChooserDialog
 import Data.Graph.Inductive.PatriciaTree
 import Data.Graph.Inductive.Graph
 import Control.Concurrent.STM
@@ -18,6 +21,8 @@ import Control.Concurrent.STM.TMChan
 import IGE.Control
 import IGE.Layout
 import IGE.Render
+import IGE.Keys
+import IGE.Serialization
 
 import Lib
 
@@ -35,7 +40,7 @@ initGr = sampleGr
 initRM :: RM
 initRM = RM (100 :+ 0) (100 :+ 100)
 
-data EditorTab = TaskEditorTab
+data EditorTab = TaskEditorTab | SysEditorTab
 
 data LabEditor = LabEditor
   { taskEditor :: TVar (EditorState Weight Weight)
@@ -47,25 +52,45 @@ newtype LabVar a = LabVar { unLabVar :: ReaderT LabEditor IO a }
 
 makeLensesFor [
   ("taskEditor", "_taskEditor")
+  , ("sysEditor", "_sysEditor")
   , ("editorTab", "_editorTab")] ''LabEditor
 
 renderLabEditor :: WidgetClass self => self -> LabEditor -> IO ()
-renderLabEditor da les = do
+renderLabEditor da le = do
   Just dw <- widgetGetWindow da
   dims    <- liftM2 (,) (drawWindowGetWidth dw) (drawWindowGetHeight dw)
-  es      <- atomically $ readTVar $ taskEditor les
+  es      <- atomically $ curTab le >>= readTVar
   renderWithDrawWindow dw $ renderEditorState es dims
 
+matchingTab :: KeyVal -> Maybe EditorTab
+matchingTab kv | kv == xK_1 = Just TaskEditorTab
+               | kv == xK_2 = Just SysEditorTab
+               | otherwise  = Nothing
+
+curTab :: LabEditor -> STM (TVar (EditorState Weight Weight))
+curTab le = do
+  et <- readTVar $ editorTab le
+  case et of
+    TaskEditorTab -> return $ taskEditor le
+    SysEditorTab  -> return $ sysEditor le
+
 keyProcessing :: ConduitT KeyVal RefreshType LabVar ()
-keyProcessing = forever $ awaitOrFinish () $ \ch -> do
-  te <- lift $ fmap taskEditor ask
-  transPipe (LabVar . withReaderT (const te) . unIGEM) $ defaultKeyBinding ch
+keyProcessing = forever $ awaitOrFinish () $ \kv -> case matchingTab kv of
+  Just t -> do
+    lift $ do
+      et <- editorTab <$> ask
+      liftIO $ atomically $ modifyTVar et $ const t
+    yield LayoutChange
+  Nothing -> do
+    ce <- lift $ ask >>= liftIO . atomically . curTab
+    transPipe (LabVar . withReaderT (const ce) . unIGEM) $ defaultKeyBinding kv
 
 refresh :: WidgetClass self => self -> ConduitT RefreshType Void LabVar ()
 refresh widget = awaitOrFinish () $ const $ do
-  te <- taskEditor <$> ask
-  liftIO $ atomically $ modifyTVar te $ \te ->
-    te { esNodeMap = layoutGr (esGraph te) }
+  le <- ask
+  liftIO $ atomically $ do
+    ce <- curTab le
+    modifyTVar ce $ \ce -> ce { esNodeMap = layoutGr (esGraph ce) }
   liftIO $ widgetQueueDraw widget
   refresh widget
 
@@ -80,6 +105,39 @@ defaultES = EditorState
   , esNodeMap = layoutGr initGr
   }
 
+openDialog :: DialogClass self => self -> IO ResponseId
+openDialog dialog = do
+  widgetShow dialog
+  response <- dialogRun dialog
+  widgetHide dialog
+  return response
+
+openGraphUsingDialog :: Window -> LabEditor -> IO ()
+openGraphUsingDialog parentWindow le = do
+  dialog <- fileChooserDialogNew
+    (Just "Open graph... " :: Maybe Text)
+    (Just parentWindow)
+    FileChooserActionOpen
+    [("gtk-cancel", ResponseCancel), ("gtk-open", ResponseAccept)]
+  response <- openDialog dialog
+  when (response == ResponseAccept) $ do
+    Just fname <- fileChooserGetFilename dialog
+    Just gr    <- graphFromBS <$> B.readFile fname
+    atomically $ curTab le >>= (`modifyTVar` \ce -> ce { esGraph = gr })
+
+saveGraphUsingDialog :: Window -> LabEditor -> IO ()
+saveGraphUsingDialog parentWindow le = do
+  dialog <- fileChooserDialogNew
+    (Just "Save graph as... " :: Maybe Text)
+    (Just parentWindow)
+    FileChooserActionSave
+    [("gtk-cancel", ResponseCancel), ("gtk-save", ResponseAccept)]
+  response <- openDialog dialog
+  when (response == ResponseAccept) $ do
+    Just fname <- fileChooserGetFilename dialog
+    es         <- atomically $ curTab le >>= readTVar
+    B.writeFile fname $ graphToBS $ esGraph es
+
 runMainWindow :: IO ()
 runMainWindow = do
   initGUI
@@ -93,12 +151,12 @@ runMainWindow = do
   task <- newTVarIO defaultES
   sys  <- newTVarIO defaultES
   tab  <- newTVarIO TaskEditorTab
-  let les = LabEditor task sys tab
+  let le = LabEditor task sys tab
 
   keyPress <- newTMChanIO
   _        <-
     forkIO
-    $  flip runReaderT les
+    $  flip runReaderT le
     $  unLabVar
     $  runConduit
     $  sourceTMChan keyPress
@@ -110,7 +168,10 @@ runMainWindow = do
     liftIO $ atomically $ writeTMChan keyPress kv
     return True
 
-  (da `on` draw) $ liftIO $ renderLabEditor da les
+  da `on` draw $ liftIO $ renderLabEditor da le
+
+  saveAs <- builderGetObject b castToImageMenuItem ("save" :: Text)
+  saveAs `on` menuItemActivated $ saveGraphUsingDialog w le
 
   widgetShowAll w
   mainGUI
